5ec9e3ec99093979d1bf8bd03438b9280535cd58
cjh8746 <cjh87467@gmail.com>
2021-07-21 23:18:21 +0900

    chore : 패키지를 컨트롤러에서 모델로 변경

--- src/main/java/MainCount.java
+++ src/main/java/MainCount.java
@@ -1,9 +1,10 @@
-import controller.SendCalculation;
 import model.Expression;
+import model.SendCalculation;
 import view.InputView;
 
 public class MainCount {
     public static void main(String[] args) {
+
         Expression expression = new Expression(InputView.inputExpression());
         SendCalculation sendCalculation = new SendCalculation(
                 expression.giveEvaluatedNumbers(),
--- src/main/java/model/SendCalculation.java
+++ src/main/java/model/SendCalculation.java
@@ -1,4 +1,4 @@
-package controller;
+package model;
 
 import model.ArithmeticOperation.Addition;
 import model.ArithmeticOperation.Division;
