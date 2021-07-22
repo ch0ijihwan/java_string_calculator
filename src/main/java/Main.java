c02d71ed6e4720238e62d2cd4a1979d7aa7d92a5
cjh8746 <cjh87467@gmail.com>
2021-07-22 15:52:24 +0900

    refactor : main 수정

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,5 +4,6 @@ public class Main {
     public static void main(String[] args) {
 
         CalculateController calculateController = new CalculateController();
+        calculateController.calculate();
     }
 }
