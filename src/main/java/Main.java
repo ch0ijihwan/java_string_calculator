588d1630f0d2fd4d6a9d6abd975dd63f6e62e13f
cjh8746 <cjh87467@gmail.com>
2021-07-22 15:34:18 +0900

    feat : 계산기 기능 수행을 위한 Main 생성

---
+++ src/main/java/Main.java
@@ -0,0 +1,9 @@
+import controller.CalculateController;
+
+public class Main {
+    public static void main(String[] args) {
+
+        CalculateController calculateController = new CalculateController();
+        System.out.println(calculateController.calculate());
+    }
+}
