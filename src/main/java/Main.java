459074723f1f3d2bbd774ff5a73eeac99cafcb73
cjh8746 <cjh87467@gmail.com>
2021-07-22 15:56:23 +0900

    style : 불필요한 줄 띄움 제거

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,7 +2,6 @@ import controller.CalculateController;
 
 public class Main {
     public static void main(String[] args) {
-
         CalculateController calculateController = new CalculateController();
         calculateController.calculate();
     }
