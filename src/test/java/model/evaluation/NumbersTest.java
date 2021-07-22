13a10ef70ef6f6034a84199d37c45700f9cf5a8d
cjh8746 <cjh87467@gmail.com>
2021-07-22 15:47:18 +0900

    chore : 패키지 변경 및 불필요한 임포트 재거

--- src/test/java/model/evaluation/NumbersTest.java
+++ src/test/java/model/evaluation/NumbersTest.java
@@ -1,4 +1,4 @@
-package model;
+package model.evaluation;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
--- src/test/java/model/evaluation/SymbolsTest.java
+++ src/test/java/model/evaluation/SymbolsTest.java
@@ -1,6 +1,5 @@
-package model;
+package model.evaluation;
 
-import model.evaluation.Symbols;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
