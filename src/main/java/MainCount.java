de8e15f863d3e58a32001df3a68cf3a7be4cc7e9
cjh8746 <cjh87467@gmail.com>
2021-07-22 14:45:00 +0900

    refactor : InputView.java 와 CalculateController 추가에 따른 MainCOunt 변경

--- src/main/java/MainCount.java
+++ src/main/java/MainCount.java
@@ -1,11 +1,14 @@
+import controller.CalculateController;
 import model.Expression;
 import model.SendCalculation;
-import view.InputView;
+import view.CalculateView;
 
 public class MainCount {
     public static void main(String[] args) {
 
-        Expression expression = new Expression(InputView.inputExpression());
+        CalculateView calculateView = new CalculateView();
+        CalculateController calculateController = new CalculateController (calculateView.inputExpression());
+        Expression expression = new Expression(calculateController.splitValues());
         SendCalculation sendCalculation = new SendCalculation(
                 expression.giveEvaluatedNumbers(),
                 expression.giveEvaluatedSymbols()
