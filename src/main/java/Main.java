9f1ee62c99017a31d04d14091fbd28de187c1852
cjh8746 <cjh87467@gmail.com>
2021-07-27 15:51:00 +0900

    refactor : Numbers를 Number(유효성 검사)와 Numbers(유효성 검사된 피연산자 리스트) 로 분리.

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,6 +3,6 @@ import controller.CalculateController;
 public class Main {
     public static void main(String[] args) {
         CalculateController calculateController = new CalculateController();
-        calculateController.calculate();
+//        calculateController.calculate();
     }
 }
---
+++ src/main/java/model/evaluation/number/Numbers.java
@@ -0,0 +1,28 @@
+package model.evaluation.number;
+
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+public class Numbers {
+    private final List<Number> numbers;
+
+    public Numbers(String[] dividedValues) {
+        numbers = IntStream.range(0, dividedValues.length)
+                .filter(Numbers::isEven)
+                .mapToObj(index -> new Number(dividedValues[index]))
+                .collect(Collectors.toList());
+    }
+
+    public int Size() {
+        return numbers.size();
+    }
+
+    private static boolean isEven(int index) {
+        return index % 2 == 0;
+    }
+
+    public int getNumber(int index) {
+        return this.numbers.get(index).value();
+    }
+}
