48f069debcada13040850897ee27050f886605d5
cjh8746 <cjh87467@gmail.com>
2021-07-29 16:08:29 +0900

    refactor : 인터페이스 구현에 따른 operate 기능 추가

--- src/main/java/model/arithmetic/Division.java
+++ src/main/java/model/arithmetic/Division.java
@@ -1,21 +1,19 @@
 package model.arithmetic;
 
-public class Division {
-    private  int number1;
-    private  int number2;
+public class Division implements Operation {
+    private final int number1;
+    private final int number2;
 
-    public Division(String number1, String number2) {
-        this.number1 = Integer.parseInt(number1);
-        this.number2 = Integer.parseInt(number2);
+    public Division(int number1, int number2) {
+        this.number1 = number1;
+        this.number2 = number2;
     }
 
-    public String Div(){
-
-        if(number2<0)
-            throw  new ArithmeticException("나눔수가 0입니다.");
-
-        String result = String.valueOf( this.number1 / this.number2);
-
-        return result;
+    @Override
+    public int operate() {
+        if (number2 == 0) {
+            throw new IllegalArgumentException("나눔수가 0 입니다");
+        }
+        return number1 / number2;
     }
 }
