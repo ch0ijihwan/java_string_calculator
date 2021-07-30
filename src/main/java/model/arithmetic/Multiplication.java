2442ccb96286a66ae8200151d1e3b237040ab475
cjh8746 <cjh87467@gmail.com>
2021-07-30 14:58:24 +0900

    style : 줄 띄움 수정

--- src/main/java/model/arithmetic/Multiplication.java
+++ src/main/java/model/arithmetic/Multiplication.java
@@ -4,10 +4,12 @@ public class Multiplication implements Operation {
 
     private int number1;
     private int number2;
+
     public Multiplication(int number1, int number2) {
         this.number1 = number1;
         this.number2 = number2;
     }
+
     public int operate() {
         return this.number1 * this.number2;
     }
