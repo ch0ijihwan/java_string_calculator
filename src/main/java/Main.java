61f4222b5253d5fb4379ea315dd879aa9b5bfdf5
cjh8746 <cjh87467@gmail.com>
2021-07-30 15:07:28 +0900

    refactor : 계산기 실행을 위한 Main 수정

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,8 +1,10 @@
 import controller.CalculateController;
+import model.evaluation.symbol.Operator;
 
 public class Main {
     public static void main(String[] args) {
         CalculateController calculateController = new CalculateController();
-//        calculateController.calculate();
+        calculateController.calculate();
+        System.out.println(Operator.values());
     }
 }
