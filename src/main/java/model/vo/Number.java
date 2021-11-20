746851fe7a467554cb4790170177285476aa8d4f
cjh8746 <cjh87467@gmail.com>
2021-11-20 23:18:18 +0900

    feat : Number 구현

---
+++ src/main/java/model/vo/Number.java
@@ -0,0 +1,13 @@
+package model.vo;
+
+public class Number {
+    private final int value;
+
+    public Number(final int value) {
+        this.value = value;
+    }
+
+    public int getValue() {
+        return value;
+    }
+}
