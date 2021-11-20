7499c439888c49357224944060b0987ad75c443c
cjh8746 <cjh87467@gmail.com>
2021-11-20 23:13:04 +0900

    test : Number 테스트 코드 수정

--- src/test/java/NumberTest.java
+++ src/test/java/NumberTest.java
@@ -1,18 +1,34 @@
 import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 class NumberTest {
-    @ParameterizedTest
+    @Test
     @DisplayName("객체 생성시, 생성자의 파라미터로 부터 입력 받은 숫자를 저장한다.")
-    @CsvSource(value = {"1"})
-    void createNumber(int input) {
+    void createNumber() {
+        //given
+        int inputNumber = 1;
+
         //when
-        Number actualNumber = new Number(input);
+        Number actualNumber = new Number(inputNumber);
 
         //then
-        assertThat(actualNumber).isEqualTo(new Number(input));
+        assertThat(actualNumber).isEqualTo(new Number(inputNumber));
+    }
+
+    @Test
+    @DisplayName("getValue() 호출 시, 객체에 저장된 값을 int 형태로 반환한다.")
+    void getNumber() {
+        //given
+        int inputNumber = 1;
+        Number number = new Number(inputNumber);
+        int expectedNumber = 1;
+
+        //when
+        int actualNumber = number.getValue();
+
+        //then
+        assertThat(actualNumber).isEqualTo(expectedNumber);
     }
 }
