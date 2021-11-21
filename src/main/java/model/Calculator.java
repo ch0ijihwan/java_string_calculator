3f784d5729c5e03acb1f07d76b2924384d0fb309
cjh8746 <cjh87467@gmail.com>
2021-11-21 18:07:20 +0900

    feat : Calculator 구현

---
+++ src/main/java/model/Calculator.java
@@ -0,0 +1,25 @@
+package model;
+
+import model.calulation.CalculationStrategy;
+import model.symbol.MathSymbol;
+
+public class Calculator {
+    private int result;
+
+    public int getResult() {
+        return result;
+    }
+    public Calculator(final Expression expression) {
+        int calculatorCount = expression.getMathSymbolsSize();
+        result = expression.nextNumber();
+        while (calculatorCount != 0) {
+            MathSymbol mathSymbol = expression.nextMathSymbol();
+            int nextNumber = expression.nextNumber();
+            CalculationStrategy calculationStrategy = MathSymbolFinder.findStrategy(mathSymbol);
+            result = calculationStrategy.calculate(result, nextNumber);
+            calculatorCount--;
+        }
+    }
+
+
+}
