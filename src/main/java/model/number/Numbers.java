8731a16ba264af1aba7c7ce31039b5c21b0561c9
cjh8746 <cjh87467@gmail.com>
2021-11-21 17:59:00 +0900

    refactor : 매직 넘버 추가 및 poll()메서드 구현

--- src/main/java/model/number/Numbers.java
+++ src/main/java/model/number/Numbers.java
@@ -5,22 +5,31 @@ import java.util.Objects;
 import java.util.stream.Collectors;
 
 public class Numbers {
-    private final List<Number> inputtedNumbers;
+    private static final int FIRST_INDEX = 0;
+    private static final int SECOND_INDEX = 1;
+    private List<Number> inputtedNumbers;
 
-    public Numbers(List<String> expression) {
+    public Numbers(final List<String> expression) {
         inputtedNumbers = expression.stream()
                 .filter(this::isInteger)
                 .map(token -> new Number(Integer.parseInt(token)))
                 .collect(Collectors.toUnmodifiableList());
     }
 
-    private boolean isInteger(String token) {
+    private boolean isInteger(final String token) {
         try {
             Integer.parseInt(token);
             return true;
         } catch (NumberFormatException e) {
             return false;
         }
+    }
+
+    public int poll() {
+        int firstNumber = inputtedNumbers.get(FIRST_INDEX).getValue();
+        int lastIndex = inputtedNumbers.size();
+        inputtedNumbers = inputtedNumbers.subList(SECOND_INDEX, lastIndex);
+        return firstNumber;
     }
 
     @Override
