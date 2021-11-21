6dcc36e643658ab368d51d058847cf4ce8a284f1
cjh8746 <cjh87467@gmail.com>
2021-11-21 15:38:25 +0900

    test : 테스트 코드 작성

---
+++ src/test/java/model/MathSymbolFinderTest.java
@@ -0,0 +1,37 @@
+package model;
+
+import model.calulation.*;
+import model.symbol.MathSymbol;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
+
+import static model.MathSymbolFinder.findStrategy;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.params.provider.Arguments.arguments;
+
+
+class MathSymbolFinderTest {
+    @ParameterizedTest
+    @DisplayName("finder() 메서드 호출 시, 파라미터로 부터 입력받은 MathSymbol 에 해당하는 계산전략을 찾아 반환한다.")
+    @MethodSource("MathSymbolsProvider")
+    void finderStrategy(MathSymbol mathSymbol, Class<CalculationStrategy> expect) {
+        //when
+        CalculationStrategy calculationStrategy = findStrategy(mathSymbol);
+
+        //then
+        assertThat(calculationStrategy.getClass()).isEqualTo(expect);
+    }
+
+    static Stream<Arguments> MathSymbolsProvider() {
+        return Stream.of(
+                arguments(MathSymbol.PLUS, Addition.class),
+                arguments(MathSymbol.MINUS, Subtraction.class),
+                arguments(MathSymbol.MULTIPLICATION, Multiplication.class),
+                arguments(MathSymbol.DIVISION, Division.class)
+        );
+    }
+}
