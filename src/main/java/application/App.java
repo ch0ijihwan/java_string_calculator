33776a235d48a20c007385c93c65ebdaee7660aa
cjh8746 <cjh87467@gmail.com>
2021-11-21 18:09:25 +0900

    feat : 실행을 위한 App 구현

---
+++ src/main/java/application/App.java
@@ -0,0 +1,9 @@
+package application;
+
+import controller.Controller;
+
+public class App {
+    public static void main(String[] args) {
+        Controller controller = new Controller();
+    }
+}
