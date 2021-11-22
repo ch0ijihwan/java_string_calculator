982d63e329870d81f6c5b3426d99ab801f4edfb3
cjh8746 <cjh87467@gmail.com>
2021-11-22 16:46:48 +0900

    refactor : 예외처리에 대한 부분 메소드로 분리

--- src/main/java/model/calulation/Division.java
+++ src/main/java/model/calulation/Division.java
@@ -3,16 +3,22 @@ package model.calulation;
 import model.symbol.MathSymbol;
 
 public class Division implements CalculationStrategy {
+    private static final int ZERO = 0;
+
     @Override
     public int calculate(final int x, final int y) {
-        if (y == 0) {
-            throw new IllegalArgumentException("숫자 0으로는 나눌 수 없습니다.");
-        }
+        isZero(y);
         return x / y;
     }
 
     @Override
     public boolean hasMathSymbol(final MathSymbol mathSymbol) {
         return MathSymbol.DIVISION == mathSymbol;
+    }
+
+    private void isZero(int dividedNumber) {
+        if (dividedNumber == ZERO) {
+            throw new IllegalArgumentException("숫자 0으로는 나눌 수 없습니다.");
+        }
     }
 }
