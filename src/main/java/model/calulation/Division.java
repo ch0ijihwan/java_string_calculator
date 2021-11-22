a8928051de8170b7a3946200ec0c86aa2b88b52b
cjh8746 <cjh87467@gmail.com>
2021-11-22 11:04:16 +0900

    refactor : 에러처리 추가

--- src/main/java/model/calulation/Division.java
+++ src/main/java/model/calulation/Division.java
@@ -5,6 +5,9 @@ import model.symbol.MathSymbol;
 public class Division implements CalculationStrategy {
     @Override
     public int calculate(final int x, final int y) {
+        if (y == 0) {
+            throw new IllegalArgumentException("숫자 0으로는 나눌 수 없습니다.");
+        }
         return x / y;
     }
 
