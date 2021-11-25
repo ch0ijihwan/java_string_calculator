db27c1daf15d681698a278b01f9c7078d2b4c56f
cjh8746 <cjh87467@gmail.com>
2021-11-25 16:34:26 +0900

    test : 테스트 코드 생성

--- src/main/java/model/Expression.java
+++ src/main/java/model/Expression.java
@@ -1,22 +1,19 @@
 package model;
 
-import model.number.Number;
+import model.calulation.CalculationStrategy;
 import model.number.Numbers;
 import model.symbol.MathSymbol;
 import model.symbol.MathSymbols;
 
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
-import java.util.stream.IntStream;
 
 public class Expression {
-    private static final int START = 0;
     private final Numbers numbers;
     private final MathSymbols mathSymbols;
 
     public Expression(final String[] inputtedExpression) {
-        validateExpression(inputtedExpression);
+        validateExpressionSize(inputtedExpression);
         numbers = new Numbers(List.of(inputtedExpression));
         mathSymbols = new MathSymbols(List.of(inputtedExpression));
     }
@@ -26,68 +23,32 @@ public class Expression {
         mathSymbols = new MathSymbols(inputtedExpression);
     }
 
-    private void validateExpression(final String[] expression) {
-        validateExpressionSize(expression);
-        validateExpressionForm(expression);
-    }
-
     private void validateExpressionSize(final String[] expression) {
         int size = expression.length;
         if (!isOddNumber(size)) {
-            throw new IllegalArgumentException("수식의 입력이 올바른 형태가 아닙니다.");
+            throw new IllegalArgumentException("수식의 입력이 잘못 되었습니다.(수식의 길이가 짝수개 입니다. 수식의 길이는 홀수개여야 합니다.)");
+        }
+        if (size < 3) {
+            throw new IllegalArgumentException("최소한의 수식이 입력되지 않았습니다.");
         }
     }
+
 
     private boolean isOddNumber(final int number) {
         return (number % 2 == 1);
     }
 
+    public int getAnswer() {
+        int answer;
 
-    private void validateExpressionForm(final String[] expression) {
-        List<String> numbersOfExpression = new ArrayList<>();
-        List<String> mathSymbolsOfExpression = new ArrayList<>();
-
-        IntStream.range(START, expression.length).filter(index -> !isOddNumber(index))
-                .forEach(index -> numbersOfExpression.add(expression[index]));
-
-        IntStream.range(START, expression.length).filter(this::isOddNumber)
-                .forEach(index -> mathSymbolsOfExpression.add(expression[index]));
-
-        if (!(validateNumberForm(numbersOfExpression) && validateMathSymbolForm(mathSymbolsOfExpression))) {
-            throw new IllegalArgumentException("수식의 형태가 이상합니다. 숫자 연산자 숫자 연산자의 반복 형태로 입력되지 않았습니다.");
+        answer = numbers.poll();
+        while (numbers.hasNext()) {
+            MathSymbol mathSymbol = mathSymbols.poll();
+            int nextNumber = numbers.poll();
+            CalculationStrategy calculationStrategy = MathSymbolFinder.findStrategy(mathSymbol);
+            answer = calculationStrategy.calculate(answer, nextNumber);
         }
-    }
-
-    private boolean validateNumberForm(final List<String> tokens) {
-        try {
-            tokens.forEach(Number::new);
-            return true;
-        } catch (IllegalArgumentException exception) {
-            return false;
-        }
-
-    }
-
-    private boolean validateMathSymbolForm(final List<String> tokens) {
-        try {
-            tokens.forEach(MathSymbol::of);
-            return true;
-        } catch (
-                IllegalArgumentException exception) {
-            return false;
-        }
-    }
-
-    public int getMathSymbolsSize() {
-        return mathSymbols.size();
-    }
-
-    public MathSymbol nextMathSymbol() {
-        return mathSymbols.poll();
-    }
-
-    public int nextNumber() {
-        return numbers.poll();
+        return answer;
     }
 
     @Override
