246f985766c83752e87dfa0e068b68e0ab41648b
cjh8746 <cjh87467@gmail.com>
2021-11-25 15:30:56 +0900

    test : 테스트 코드 수정

--- src/test/java/model/number/NumbersTest.java
+++ src/test/java/model/number/NumbersTest.java
@@ -8,39 +8,19 @@ import java.util.Arrays;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.assertAll;
 
 class NumbersTest {
     @Test
-    @DisplayName("생성자로부터 식을 입력 받으면, 입력 받은 값들 중 숫자만 Number 형태의 리스트로 저장한다.")
+    @DisplayName("객체 생성시, 생성자의 파라미터로 부터 입력받은 리스트 값들 중 홀수 번째 인덱스 값들을 Number 객체화 하여 Queue 형태로 저장한다.")
     void createNumbers() {
         //given
         List<String> inputExpression = Arrays.asList("1", "+", "2");
-        List<String> expected = Arrays.asList("1", "2");
+        List<String> expected = Arrays.asList("1", " ", "2");
 
         //when
         Numbers numbers = new Numbers(inputExpression);
 
         //then
         assertThat(numbers).isEqualTo(new Numbers(expected));
-    }
-
-    @Test
-    @DisplayName("poll() 호출 시, 리스트의 제일 앞 값을 삭제하고, 값을 반환한다.")
-    void poll() {
-        //given
-        List<String> inputtedExpression = Arrays.asList("1", "+", "3", "-", "2");
-        Numbers numbers = new Numbers(inputtedExpression);
-        int expectedNumber = 1;
-        Numbers expectedNumbers = new Numbers(Arrays.asList("3", "2"));
-
-        //when
-        int actualNumber = numbers.poll();
-
-        //then
-        assertAll(
-                () -> assertThat(actualNumber).isEqualTo(expectedNumber),
-                () -> assertThat(numbers).isEqualTo(expectedNumbers)
-        );
     }
 }
